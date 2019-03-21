d2a658f639644ec94859474da2d6c0381c3ce611
kh <kh.jung@eggbun-edu.com>
2019-03-21 21:24:54 +0900

    BestAlbum Spy

--- src/test/kotlin/programmers/HashBestAlbumTest.kt
+++ src/test/kotlin/programmers/HashBestAlbumTest.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 // https://programmers.co.kr/learn/courses/30/lessons/42579?language=kotlin
 class HashBestAlbumTest {
     @Test fun test01() {
-        val r = algorithm(
+        val r = Solution().solution(
             arrayOf("classic", "pop", "classic", "classic", "pop"),
             intArrayOf(500, 600, 150, 800, 2500)
         )
@@ -18,7 +18,7 @@ class HashBestAlbumTest {
     }
 
     @Test fun test02() {
-        val r = algorithm(
+        val r = Solution().solution(
             arrayOf   ("classic", "pop", "classic", "classic", "pop", "jazz"),
             intArrayOf( 500,       600,   150,       800,       100,   2500)
         )
@@ -31,7 +31,7 @@ class HashBestAlbumTest {
     }
 
     @Test fun test03() {
-        val r = algorithm(
+        val r = Solution().solution(
             arrayOf   ("classic", "pop", "classic", "pop", "classic", "classic"),
             intArrayOf( 400,       600,   150,       2500,  500,       500)
         )
@@ -43,7 +43,7 @@ class HashBestAlbumTest {
     }
 
     @Test fun test04() {
-        val r = algorithm(
+        val r = Solution().solution(
             arrayOf   ("classic", "pop", "jazz"),
             intArrayOf( 400,       600,   150)
         )
@@ -54,7 +54,7 @@ class HashBestAlbumTest {
     }
 
     @Test fun test05() {
-        val r = algorithm(
+        val r = Solution().solution(
             arrayOf   ("classic", "pop", "jazz"),
             intArrayOf( 400,       600,   150)
         )
@@ -64,29 +64,58 @@ class HashBestAlbumTest {
         assertThat(r[2]).isEqualTo(2)
     }
 
-    private fun algorithm(genres: Array<String>, plays: IntArray): IntArray {
-        val genresList = genres.toList()
-        val playsList = plays.toList()
+    @Test fun test06() {
+        val r = Solution().solution(
+            arrayOf   ("classic", "jazz", "classic", "pop", "classic", "classic"),
+            intArrayOf( 500,       600,    150,       2500,  500,       500)
+        )
 
-        val summed = genresList.zip(playsList)
-            .asSequence()
-            .associateBy { it.first }
-            .toList()
-            .sortedByDescending { it.second.second }
-            .map { it.second }
+        assertThat(r[0]).isEqualTo(3)
+        assertThat(r[1]).isEqualTo(0)
+        assertThat(r[2]).isEqualTo(4)
+        assertThat(r[3]).isEqualTo(1)
+    }
 
-        val zipped = genresList
-            .zip(playsList)
-            .mapIndexed { index, pair ->  Triple(pair.first, pair.second, index) }
-
-        val answer = summed.flatMap { _sum -> zipped.filter { _zipped -> _zipped.first == _sum.first }
-            .asSequence()
-            .toList()
-            .sortedByDescending { it.second }
-            .map { it.third }
-            .take(2)
+    class Solution {
+        fun solution(genres: Array<String>, plays: IntArray): IntArray {
+            return genres.indices.groupBy { genres[it] }
+                .toList()
+                .sortedByDescending { it.second.sumBy { plays[it] } }
+                .map { it.second.sortedByDescending { plays[it] }.take(2) }
+                .flatten()
+                .toIntArray()
         }
 
-        return answer.toIntArray()
+        fun solution2(genres: Array<String>, plays: IntArray): IntArray {
+            val genresList = genres.toList()
+            val playsList = plays.toList()
+
+            val summed = genresList.zip(playsList)
+                .fold(hashMapOf<String, Int>()) { acc, pair ->
+                    if (acc.containsKey(pair.first)) {
+                        acc[pair.first] = pair.second + acc[pair.first]!!
+                    } else {
+                        acc[pair.first] = pair.second
+                    }
+
+                    acc
+                }
+                .toList()
+                .sortedByDescending { it.second }
+
+            val zipped = genresList
+                .zip(playsList)
+                .mapIndexed { index, pair ->  Triple(pair.first, pair.second, index) }
+
+            val answer = summed.flatMap { _sum -> zipped.filter { _zipped -> _zipped.first == _sum.first }
+                .asSequence()
+                .toList()
+                .sortedByDescending { it.second }
+                .map { it.third }
+                .take(2)
+            }
+
+            return answer.toIntArray()
+        }
     }
 }
--- src/test/kotlin/programmers/HashSpyTest.kt
+++ src/test/kotlin/programmers/HashSpyTest.kt
@@ -6,9 +6,9 @@ import org.junit.Test
 /**
  * https://programmers.co.kr/learn/courses/30/lessons/42578?language=kotlin
  * start 15:20
- * end
+ * end 20:53
  */
-class HashCamouflageTest {
+class HashSpyTest {
 
     @Test fun test01() {
         val r = Solution().solution(
@@ -38,15 +38,16 @@ class HashCamouflageTest {
         val r = Solution().solution(
             arrayOf(
                 arrayOf("blue_sunglasses", "eyewear"),
+
                 arrayOf("yellow_hat", "headgear"),
                 arrayOf("green_turban", "headgear"),
+
                 arrayOf("crow_mask", "face"),
-                arrayOf("blue_sunglasses", "face"),
                 arrayOf("smoky_makeup", "face")
             )
         )
 
-        assertThat(r).isEqualTo(12)
+        assertThat(r).isEqualTo(17)
     }
 
     @Test fun test04() {
@@ -69,15 +70,8 @@ class HashCamouflageTest {
     class Solution {
         fun solution(clothes: Array<Array<String>>): Int {
             val grouped = clothes.map { Pair(it[0], it[1]) }.groupBy { it.second }.map { Pair(it.key, it.value.size) }
-                //.also { println(it) }
 
-            return if (grouped.size == 1) {
-                clothes.size
-            } else {
-                clothes.size.also { println(it) }
-                + grouped.fold(1) { acc, count -> acc * count.second}.also { println(it) }
-                + grouped.map { it.second }.zipWithNext().fold(1) { acc, count -> acc * count.first * count.second }.also { println(it) }
-            }
+            return grouped.map { it.second }.fold(1) { acc, i -> acc * (i + 1) } - 1
         }
     }
 }
