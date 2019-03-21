b9d9a45c06ebdd3b5639eef0ce9ef1977e04ecf7
kh <kh.jung@eggbun-edu.com>
2019-03-21 17:55:31 +0900

    아 3을 못푸네 -_-

--- src/test/kotlin/programmers/DFSNetworkTest.kt
+++ src/test/kotlin/programmers/DFSNetworkTest.kt
@@ -2,11 +2,12 @@ package programmers
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import java.util.*
 
 /**
  * https://programmers.co.kr/learn/courses/30/lessons/43162?language=kotlin
  * start 15:14
- * end
+ * end GG
  */
 class DFSNetworkTest {
 
@@ -28,36 +29,42 @@ class DFSNetworkTest {
         assertThat(r).isEqualTo(3)
     }
 
+    @Test fun test04() {
+        val r = Solution().solution(4, arrayOf(intArrayOf(1, 0, 0, 0), intArrayOf(0, 1, 0, 0), intArrayOf(0, 0, 1, 0), intArrayOf(0, 0, 0, 1)))
+
+        assertThat(r).isEqualTo(4)
+    }
+
     class Solution {
-        private val r = arrayListOf<MutableSet<Int>>()
-        private var count = 0
+        private val result = arrayListOf<MutableSet<Int>>()
 
         fun solution(n: Int, computers: Array<IntArray>): Int {
-            count = n
+            dfs(computers.map { it.toList() }.toList(), n - 1)
 
-            dfu(computers, 0, 0)
-
-            return r.size
+            return result.size
         }
 
-        private fun dfu(p: Array<IntArray>, i: Int, j: Int) {
-            if (i == count - 1 && j == count - 1) return
+        private fun dfs(p: List<List<Int>>, n: Int) {
 
-            if (p[i][j] == 1) {
-                val set = r.find { it.contains(i) || it.contains(j) }
-                if (set == null) {
-                    val newSet = mutableSetOf<Int>()
-                    newSet.add(i)
-                    newSet.add(j)
-                    r.add(newSet)
-                } else {
-                    set.add(i)
-                    set.add(j)
+            for (i in IntRange(0, n)) {
+                val temp = p[i]
+
+                temp.forEachIndexed { j, value ->
+                    if (value == 1) {
+                        val set = result.find { set -> set.find { it == i } != null || set.find { it == j } != null }
+
+                        if (set == null) {
+                            val newSet = mutableSetOf<Int>()
+                            newSet.add(i)
+                            newSet.add(j)
+                            result.add(newSet)
+                        } else {
+                            set.add(i)
+                            set.add(j)
+                        }
+                    }
                 }
             }
-
-            dfu(p, i + 1, j)
-            dfu(p, i, j + 1)
         }
     }
 }
